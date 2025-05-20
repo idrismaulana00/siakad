80c6117dff779b75e6a26c5f5cb1826a6d04ebdf
idrisjogja <idrismaulana2204@gmail.com>
2025-05-20 15:10:46 +0700

    new image

--- lib/main.dart
+++ lib/main.dart
@@ -24,33 +24,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(onGenerateRoute: (seting) {
-      return MaterialPageRoute(
-          settings: seting,
-          builder: (context) {
-            if (seting.name == 'nilai')
-              return NilaiPage();
-            else if (seting.name == 'absensi')
-              return AbsensiPage();
-            else if (seting.name == 'jadwal')
-              return JadwalPage();
-            else if (seting.name == 'pembayaran')
-              return PembayaranPage();
-            else if (seting.name == 'pencapainkuliah')
-              return PencapainKuliahPage();
-            else if (seting.name == 'pengumuman')
-              return PengumumanPage();
-            else if (seting.name == 'sertifikasi')
-              return SertifikasiPage();
-            else if (seting.name == 'wisuda')
-              return WisudaPage();
-            else if (seting.name == 'profil')
-              return ProfilPage();
-            else if (seting.name == 'krs')
-              return KrsPage();
-            else
-              return HomePage();
-          });
-    });
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: (seting) {
+          return MaterialPageRoute(
+              settings: seting,
+              builder: (context) {
+                if (seting.name == 'nilai')
+                  return NilaiPage();
+                else if (seting.name == 'absensi')
+                  return AbsensiPage();
+                else if (seting.name == 'jadwal')
+                  return JadwalPage();
+                else if (seting.name == 'pembayaran')
+                  return PembayaranPage();
+                else if (seting.name == 'pencapainkuliah')
+                  return PencapainKuliahPage();
+                else if (seting.name == 'pengumuman')
+                  return PengumumanPage();
+                else if (seting.name == 'sertifikasi')
+                  return SertifikasiPage();
+                else if (seting.name == 'wisuda')
+                  return WisudaPage();
+                else if (seting.name == 'profil')
+                  return ProfilPage();
+                else if (seting.name == 'krs')
+                  return KrsPage();
+                else
+                  return HomePage();
+              });
+        });
   }
 }
